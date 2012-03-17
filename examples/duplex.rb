e93da9911bd166e620caaa36888df53078096170
Ari Russo <ari.russo@gmail.com>
2012-03-17 11:33:27 -0700

    duplex example

--- examples/duplex.rb
+++ examples/duplex.rb
@@ -8,9 +8,7 @@ OSC.using(:input_port => 8000, :output => { :host => "localhost", :port => 8000 
   receive("/greeting") { |val| p "received #{val}" }
 
   p "Ready to receive OSC messages on port(s) #{input_ports.join(', ')}..."
-  
-  sleep(0.5) # give the receiver thread some time to fully start
-    
+
   out "/greeting", "hullo!"
   
   wait_for_input
