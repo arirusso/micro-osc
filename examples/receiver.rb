0bbb107ebc0c49760f9ebaa4321365034674659b
Ari Russo <ari.russo@gmail.com>
2012-03-17 09:56:15 -0700

    receiver-only example

--- examples/receiver.rb
+++ examples/receiver.rb
@@ -3,12 +3,12 @@ $:.unshift File.join( File.dirname( __FILE__ ), '../lib')
 
 require "osc"
 
-OSC.using(:input_port => 8000, :output => { :host => "localhost", :port => 9000 }) do
-
-  out("/greeting", "hello!")
+OSC.using(:input_port => 8000) do
   
   receive("/greeting") { |val| p "received #{val}" }
-  
-  join
+
+  p "Ready to receive OSC messages on port(s) #{input_ports.join(', ')}..."
+    
+  wait_for_input
 
 end
